6f5cf2b3623bbd0befb7ab20d5d23722e163400a
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-15 02:54:38 +0300

    task2 realization

---
+++ task2.psm1
@@ -0,0 +1,47 @@
+function DirInf {
+    param (
+        [String] $Path = "."
+    )
+    $DirectoriesCount = (Get-ChildItem $Path -Directory | Measure-Object).Count
+    $FilesCount = (Get-ChildItem $Path -File | Measure-Object).Count
+    return @{
+        "DirectoriesCount" = $DirectoriesCount;
+        "FilesCount" = $FilesCount;
+    }
+}
+
+function CurDirInf {
+    param (
+        [String] $Path = ".",
+        [Int16] $DepthLevel = 0
+    )
+    $DirectoryName = $(Resolve-Path $Path).Path
+    $Offset = ""
+    for ($Depth = 0; $Depth -lt $DepthLevel; $Depth++) {
+        $Offset += "  "
+    }
+    return "$Offset $DepthLevel $DirectoryName"
+}
+
+function GoDirs {
+    param (
+        [String] $Path = ".",
+        [Int16] $DepthLevel = 0
+    )
+    $DirectoryInfo = DirInf -Path $Path
+    $DirectoryOutput = `
+        "$(CurDirInf -Path $Path -DepthLevel $DepthLevel)" `
+        + " | Dirs - $($DirectoryInfo.DirectoriesCount)" `
+        + " | Files - $($DirectoryInfo.FilesCount)"
+    Write-Host $DirectoryOutput
+    $SubDirectories = Get-ChildItem $Path -Directory -Name
+    foreach ($SubDirectory in $SubDirectories) {
+        $SubDirectoryPath = Join-Path $Path $SubDirectory
+        $SubDirectoryDepthLevel = $DepthLevel + 1
+        GoDirs -Path $SubDirectoryPath -DepthLevel $SubDirectoryDepthLevel
+    }
+}
+
+Export-ModuleMember DirInf
+Export-ModuleMember CurDirInf
+Export-ModuleMember GoDirs
