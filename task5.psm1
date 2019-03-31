4fe2142e2d40eecc2faef733c5a72d6cfcdb1bbe
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-31 18:21:40 +0300

    task5 implementation

--- task5.psm1
+++ task5.psm1
@@ -2,3 +2,15 @@
 Вывести в порядке возрастания для процессов, у которых свойство Handles больше указанной величины 
 (задать как параметр), в виде таблицы Handles, Name, Description
 #>
+
+function Get-FormattedProcessesTable {
+    param (
+        [int16] $Handles = 0
+    )
+    Get-Process `
+        | Where-Object { $_.Handles -gt $Handles } `
+        | Sort-Object Handles `
+        | Format-Table Handles, Name, Description
+}
+
+Export-ModuleMember Get-FormattedProcessesTable
