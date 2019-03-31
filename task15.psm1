4355e8423c443a7e767f95e92e1ffb77078e155c
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-31 20:37:11 +0300

    task15 implementation

--- task15.psm1
+++ task15.psm1
@@ -2,3 +2,17 @@
 Даны два целочисленных массива. 
 Удалить из первого массива все элементы содержащиеся во втором массиве
 #>
+
+function Get-ArrayDifference {
+    param (
+        [array] $First,
+        [array] $Second
+    )
+    $ArrayDifference = $First
+    foreach ($SecondArrayElement in $Second) {
+        $ArrayDifference = $ArrayDifference | Where-Object { $_ -ne $SecondArrayElement }
+    }
+    return $ArrayDifference
+}
+
+Export-ModuleMember Get-ArrayDifference
