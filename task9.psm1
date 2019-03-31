d32f35b5910eca7b6066a0fedbc0e8f36f03de64
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-31 19:25:01 +0300

    task9 implementation

--- task9.psm1
+++ task9.psm1
@@ -1,3 +1,12 @@
 <#
 Функция устранения в числовом массиве повторяющихся элементов
 #>
+
+function Remove-Duplicates {
+    param (
+        [array] $Array
+    )
+    return $Array | Sort-Object -Unique
+}
+
+Export-ModuleMember Remove-Duplicates
