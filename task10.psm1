36b0deec86391b41f5c6f4bbcb6fd70a17dc365a
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-31 19:45:42 +0300

    task10 implementation

--- task10.psm1
+++ task10.psm1
@@ -1,3 +1,16 @@
 <#
-Функция определения количества дней между двумя датами, которые задаются как параметры.
+Функция определения количества дней между двумя датами, 
+которые задаются как параметры.
 #>
+
+function Get-DaysBetweenDates {
+    param (
+        [datetime] $Start,
+        [datetime] $End
+    )
+    return [System.Math]::Abs(
+        (New-TimeSpan -Start $Start -End $End).Days
+    )
+}
+
+Export-ModuleMember Get-DaysBetweenDates
