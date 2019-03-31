21c1595149f24af595c0fe02ca97e819e3f79b51
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-31 23:31:18 +0300

    task18 implementation

--- task18.psm1
+++ task18.psm1
@@ -1,3 +1,23 @@
 <#
-Создать из powershell в Excel три строки: фамилия, имя телефон
+Создать из powershell в Excel три строки: фамилия, имя, телефон
 #>
+
+function Write-RowsToExcel {
+    param (
+        [System.IO.FileInfo] $FilePath,
+        [array] $Rows = @('Name', 'Surname', 'Phone')
+    )
+    $ExcelApplication = New-Object -ComObject excel.application
+    $ExcelApplication.Visible = $true
+    $ExcelWorkBook = $ExcelApplication.Workbooks.Add()
+    $WorkSheet = $ExcelWorkBook.Worksheets.Item(1)
+    $WorkSheet.Name = 'Contacts'
+    $CurrentCell = 1
+    foreach ($Row in $Rows) {
+        $WorkSheet.Cells(1, $CurrentCell++) = $Row
+    }
+    $WorkSheet.SaveAs([ref] $FilePath.FullName)
+    $ExcelApplication.Quit()
+}
+
+Export-ModuleMember Write-RowsToExcel
