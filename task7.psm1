6b3f03daea6f13e639562bff808b9a19e4d0dcc9
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-31 19:18:17 +0300

    task7 implementation

--- task7.psm1
+++ task7.psm1
@@ -2,3 +2,14 @@
 Функция удаления файла (имя задается как параметр функции) 
 из заданного каталога (задается в качестве параметра). 
 #>
+
+function Remove-FileFromFolder {
+    param (
+        [string] $File,
+        [string] $Folder = "."
+    )
+    $FileAbsolutePath = Join-Path (Resolve-Path $Folder).Path $File
+    Remove-Item $FileAbsolutePath
+}
+
+Export-ModuleMember Remove-FileFromFolder
