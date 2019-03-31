37f6be3818b0f21ca196ca12173478f035e392a3
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-31 22:26:32 +0300

    task17 implementation

--- task17.psm1
+++ task17.psm1
@@ -1,3 +1,16 @@
 <#
 Создать ярлык на рабочем столе
 #>
+
+#Requires -RunAsAdministrator
+
+function Add-DesktopLink {
+    param (
+        [System.IO.FileInfo] $Target
+    )
+    $TargetNameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($Target.FullName)
+    $LinkPath = [System.IO.Path]::Combine("~", "Desktop", $TargetNameWithoutExtension)
+    New-Item -Path $LinkPath -ItemType SymbolicLink -Value $Target
+}
+
+Export-ModuleMember Add-DesktopLink
