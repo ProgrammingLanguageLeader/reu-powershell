032637f9b89cd1e2999661384ef25a9a07a5b1b7
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-31 20:03:53 +0300

    task11 implementation

--- task11.psm1
+++ task11.psm1
@@ -1,5 +1,24 @@
 <#
-Функция определения даты и дня недели, отстоящей от заданной на указанное количество дней (со знаком + или -). 
-Если день недели Суббота или Воскресенье его выдать красным цветом, в противном случае желтым. 
-Начальная дата параметр функции, при его отсутствии за дату считать текущую. 
+Функция определения даты и дня недели, отстоящей от 
+заданной на указанное количество дней (со знаком + или -). 
+Если день недели  - Суббота или Воскресенье, его выдать красным 
+цветом, в противном случае желтым. Начальная дата - параметр 
+функции, при его отсутствии за дату считать текущую. 
 #>
+
+function Get-DateInfoByOffset {
+	param(
+		[int] $DaysOffset,
+		[datetime] $Date = $(Get-Date)
+	)
+	$OffsetDate = $Date.AddDays($DaysOffset)
+	$DayOfWeek = $OffsetDate.DayOfWeek
+	$Color = "Yellow"
+	if ($DayOfWeek -eq 0 -or $DayOfWeek -eq 6) {
+		$Color = "Red"
+	}
+	Write-Host -ForegroundColor $color $DayOfWeek -NoNewline
+	Write-Host " $OffsetDate"
+}
+
+Export-ModuleMember Get-DateInfoByOffset
