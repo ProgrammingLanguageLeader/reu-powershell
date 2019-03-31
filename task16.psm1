2d1edf815c990a61003321ccfb7304c3a1ac0dee
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-31 21:00:55 +0300

    task16 implementation

--- task16.psm1
+++ task16.psm1
@@ -1,3 +1,18 @@
 <#
 Из Word выдать строку «Привет из powershell»
 #>
+
+function Write-MessageToWord {
+    param (
+        [string] $FilePath,
+        [string] $Message = "Hello from PowerShell!"
+    )
+    $WordApplication = New-Object -ComObject word.application
+    $WordApplication.Visible = $true
+    $Document = $WordApplication.Documents.Add()
+    $WordApplication.Selection.TypeText($Message)
+    $Document.SaveAs([ref] $FilePath)
+    $WordApplication.Quit()
+}
+
+Export-ModuleMember Write-MessageToWord
